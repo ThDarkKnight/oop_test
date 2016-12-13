8a1285f7944034cdcc4bf397dc6bcf86ec4b1bb9
ThDarkKnight <onurhan.hamzaoglu@gmail.com>
2016-12-13 14:59:30 +0300

    Revert "Add first source file"
    This reverts commit bc3d64ba27c737f469a7beffdcdf2a6d57f6a522.
    
    deneme

--- div2zero_v2.cpp
+++ div2zero_v2.cpp
@@ -1,26 +1,54 @@
 #include <iostream>
 using namespace std;
 
+#ifdef DOUBLE
+#define MYTYPE double
+#elif FLOAT
+#define MYTYPE float
+#else
+#define MYTYPE int
+#endif
+
+template<class cTYPE>
+class myerr {
+  private:
+    cTYPE x, y;
+
+  public:
+    myerr(cTYPE a, cTYPE b) {
+      x = a;
+      y = b;
+    }
+
+    void print() {
+      cout << "x,y pair = " << x << " " << y << endl;
+    }
+};
+
 template<class dTYPE>
-dTYPE div(dTYPE x, dTYPE y) {
-  if (y == (dTYPE) 0.0) {
-    throw "division by zero !!!";
+double mydiv(dTYPE x, dTYPE y) throw(myerr<dTYPE>) {
+  dTYPE ZERO = (dTYPE) 0;
+  if (y == ZERO) {
+    myerr<dTYPE> e(x,y);
+    throw e;
   }
-  return x/y;
+  return (double)x/(double)y;
 }
 
 int main() {
-  double x = 0.0;
-  double y = 0.0;
+  //İlk github değişikliğimiz... :)
+  // init variables
+  MYTYPE x = 0;
+  MYTYPE y = 0;
 
   cout << "Enter x,y pair = " << endl;
   cin >> x >> y;
 
   double z = 0.0;
   try {
-    z = div(x,y);   
-  } catch (const char * str) {
-    cout << str << endl;
+     z = mydiv(x,y);
+  } catch (myerr<MYTYPE> &e) {
+    e.print();
     return -1;
   }
 
